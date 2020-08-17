a094c78b87353219a50db08870597e0eb834eb2d
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-08-17 19:00:07 +0900

    Start Task3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = User.find(current_user.id)
+    @book_comment = BookComment.new
   end
 
   def index
